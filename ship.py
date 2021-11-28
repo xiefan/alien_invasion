9fe06c467e8b7571a8c04c4898e57174a946e80f
AndyG6 <zeyuguo2006@gmail.com>
2021-11-28 15:40:23 -0800

    added movement now onto speed

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         # initialize the ship and set its starting position
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
@@ -16,15 +17,22 @@ class Ship:
         self.rect = self.image.get_rect()
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
-        # self.rect = pygame.display.set_mode((150,150))
+
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
 
         # Movement flag
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        # Update the ship's position based on the movement flag
+        # Update the ship's position based on the movement flags
         if self.moving_right:
-            self.rect.x += 1
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left:
+            self.rect.x -= self.settings.ship_speed
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         # Draw the ship at its current location
