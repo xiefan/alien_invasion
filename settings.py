d3e4598b602fb76ed37bf937f3d9b0bb760e5b08
AndyG6 <zeyuguo2006@gmail.com>
2021-12-20 18:03:44 -0800

    Made the game fullscreen, added shooting bullets

--- settings.py
+++ settings.py
@@ -8,5 +8,10 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # Ship settings
-        self.ship_speed = 0.5
-        
+        self.ship_speed = 1
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+
